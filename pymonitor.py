b3d7c7f6885d1ae0ab6b6a54af8daf54585acf92
John Cronin <johncronin@scifa.co.uk>
2011-06-04 15:51:32 +0100

    Added support for querying the SMART status of the installed drives

--- pymonitor.py
+++ pymonitor.py
@@ -5,6 +5,7 @@ from daemon3x import daemon
 import monitor
 import send_thecus
 import time
+import smartctl_interface
 
 parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
 parser.add_option("-p", "--port", action="store", type="string", dest="port",
@@ -19,11 +20,19 @@ parser.add_option("--raid-device", action="store", type="string", dest="rdev",
 		help="RAID device to monitor (default is first listed in mdstat)")
 parser.add_option("--stop", action="store_true", dest="stop",
 		default=False, help="stop a running daemon")
+parser.add_option("-S", "--smartctl", action="store_true", dest="smartctl",
+		default=False, help="use smartctl to query the states of the drives")
+parser.add_option("--smartctl-delay", action="store", type="int", dest="smartctldelay", help="number of seconds before rechecking SMART status", default=60)
 opts, args = parser.parse_args()
 
 def main_loop():
+	smarttimer = 0
+
 	while True:
 		msg1 = time.strftime("%d-%b-%Y %H:%M:%S")
+
+		if(opts.smartctl and smarttimer == 0):
+			raid_msg = smartctl_interface.get_smart_info()
 
 		raid_db = monitor.get_status()
 		try:
@@ -39,6 +48,9 @@ def main_loop():
 		except:
 			msg2 = "RAID: not found"
 
+		if(opts.smartctl and smarttimer % 2 == 0):
+			msg2 = raid_msg
+
 		if opts.debug == True:
 			print(msg1)
 			print(msg2)
@@ -46,6 +58,10 @@ def main_loop():
 		else:
 			send_thecus.write_message(msg1 = msg1, msg2 = msg2,
 					port = opts.port)
+
+		smarttimer += 1
+		if(smarttimer == opts.smartctldelay):
+			smarttimer = 0
 
 		time.sleep(1)
 
