3566732660162da06a4015f3b1a91f57a98b7970
John Cronin <johncronin@scifa.co.uk>
2013-08-26 18:07:20 +0100

    Removed code to determine which drives were in which raid array.  Was not being used and caused errors with drives which weren't present

--- raid_interface.py
+++ raid_interface.py
@@ -15,14 +15,6 @@ def get_status():
 		entry = raid_entry(device[0], device[1], device[2], [],
 				device[4], "/dev/%s" % device[0])
 	
-		for drive in re.findall(r"[\w\[\]]+", device[3]):
-			src_res = re.search(r"(\w+?)\[(\d+)\]", drive)
-
-			sys_fd = open("/sys/block/%s/device/model" % src_res.group(1), "r")
-			entry.drives.append(drive_entry(src_res.group(1),
-				src_res.group(2), sys_fd.readline().rstrip()))
-			sys_fd.close
-
 		raid_dict[entry.name] = entry
 
 	return raid_dict
